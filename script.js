e13fbfd88ae804e0b66f030957f8a3e798bfc0cb
aparna <aparnaann2000@gmail.com>
2021-04-10 12:17:35 +0530

    First Commit : First Version

---
+++ script.js
@@ -0,0 +1,38 @@
+ $(document).ready(function() {
+    $("button").click(function() {
+
+        var user = $("#username").val();
+        var admin = "admin"
+        var pass = "12345";
+        var pas = $("#password").val();
+        if (user == admin && pas == pass) {
+            window.location.href = "home.html"
+            return false;
+        } else {
+            alert("Username/Password is incorrect");
+        }
+    });
+});
+
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: false })
+    .add({
+        targets: '.ml3 .letter',
+        opacity: [0, 1],
+        easition: 5250,
+        delay: (el, i) => 150 * (i + 1)
+    }).add({
+        
+    });
+
+
+function preventBack() {
+    window.history.forward();
+
+}
+setTimeout("preventBack()", 0);
+window.onunload = function() {
+    null;
+}
